e7a484f88527dc37336c9a912e311e63f64c4491
hhs0579 <hhs0579@naver.com>
2025-07-12 18:36:06 +0900

    dd

--- quest/lib/pages/Desktop/desktop6.dart
+++ quest/lib/pages/Desktop/desktop6.dart
@@ -17,13 +17,123 @@ class Desktop6 extends StatelessWidget {
                 minHeight: MediaQuery.of(context).size.height,
               ),
               width: MediaQuery.of(context).size.width,
-              child:
-              Column(
-                children: [
-                  
-                ],
-              )
-              );
+              child: Container(
+                padding: EdgeInsets.only(top: 60, bottom: 60),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      '실제 사용 후기',
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      '선생님들과 학생들 모두 추천하는',
+                      style: TextStyle(
+                          fontSize: 48,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      '만족도 4.8의 퀘스트스쿨!',
+                      style: TextStyle(
+                          fontSize: 48,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 24,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              '만족도',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                  fontSize: 24),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  child: Image.asset('assets/images/i9.png'),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  '5.0',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 48),
+                                )
+                              ],
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Container(
+                          width: 1,
+                          height: 32,
+                          color: Color(0xffD9D9D9),
+                        ),
+                        SizedBox(
+                          width: 50,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              '추천 지수',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  color: Colors.white,
+                                  fontSize: 24),
+                            ),
+                            SizedBox(
+                              height: 8,
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  width: 40,
+                                  height: 40,
+                                  child: Image.asset('assets/images/i10.png'),
+                                ),
+                                SizedBox(
+                                  width: 8,
+                                ),
+                                Text(
+                                  '100%',
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                      fontSize: 48),
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ));
         },
       ),
     );
